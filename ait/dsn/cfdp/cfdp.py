4b54edee796fc67f84941f57ff82b88cb1c41f36
Anna Waldron <anna.waldron@jpl.nasa.gov>
2019-08-23 09:30:32 -0700

    Update CFDP disconnect error handling (#73)

--- ait/dsn/cfdp/cfdp.py
+++ ait/dsn/cfdp/cfdp.py
@@ -112,11 +112,12 @@ class CFDP(object):
         try:
             self._rcvr_socket.close()
             self._sender_socket.close()
-            self._receiving_handler.kill()
-            self._sending_handler.kill()
-            self.mib.dump()
         except Exception:
             pass
+
+        self._receiving_handler.kill()
+        self._sending_handler.kill()
+        self.mib.dump()
 
     def _increment_tx_counter(self):
         self.transaction_counter += 1
