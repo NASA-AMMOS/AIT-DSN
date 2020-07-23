5c55acbbbdd9a3fc9cd0d81aee42e62e3b55e4f2
kmarwah <kmarwah@users.noreply.github.com>
2020-07-23 16:17:13 -0700

    move servery.py

--- ait/dsn/bin/server.py
+++ ait/dsn/bin/server.py
@@ -8,8 +8,9 @@ Maybe change this from a nested dictionary to a class? Issues:
 -passing class instance to the function which is decorated by the bottle endpoints– want to avoid global objects
 -accessing subclasses (interfaces), because the decorator is currently passing the interface name as a string to the handler function
 
-Possibility: make the handler functions members of the main class? Ask Michael. 
+Possibility: make the handler functions members of the main class? Ask Michael.
 '''
+
 DSN_harness = {
     'RAF': {
         'instance': ait.dsn.sle.RAF,
