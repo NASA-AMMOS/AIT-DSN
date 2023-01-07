49c179ecaae36e2ab0efbb3b278bb0b3240b1c87
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2023-01-07 14:51:16 -0800

    Clean up depacketizer

--- ait/dsn/plugins/AOS_to_CCSDS.py
+++ ait/dsn/plugins/AOS_to_CCSDS.py
@@ -7,6 +7,13 @@ from collections import OrderedDict, namedtuple
 from colorama import Fore, Back, Style
 import time
 
+
+class Packet_State(Enum):
+    COMPLETE = auto()
+    UNDERFLOW = auto()
+    SPILLOVER = auto()
+    IDLE = auto()
+
 class HeaderKeys(Enum):
     PACKET_VERSION_NUMBER = slice(0, 3)
     PACKET_TYPE = slice(3, 4)
@@ -15,7 +22,7 @@ class HeaderKeys(Enum):
     SEQUENCE_FLAGS = slice(16, 18)
     PACKET_SEQUENCE_OR_NAME = slice(18, 32)
     PACKET_DATA_LENGTH = slice(32, 48)
-    
+
 
 class CCSDS_Packet():
 
@@ -36,24 +43,24 @@ class CCSDS_Packet():
         else:
             self.primary_header[HeaderKeys.PACKET_DATA_LENGTH.name] = PACKET_DATA_LENGTH
         self.secondary_header = {}
-        
+
         self.encoded_packet = bytes()
+        self.error = None
 
     @staticmethod
     def decode(packet_bytes):
-        #print()
-        #print(f"{packet_bytes=}")
-        data_length = int.from_bytes(packet_bytes[4:6], 'big') 
-        #print(f"{data_length=}")
+        data_length = int.from_bytes(packet_bytes[4:6], 'big')
         if not data_length: # regular check is apid 111111....
-            return None
-        
+            #log.warn("Underflow: Insufficient Data")
+            return (Packet_State.UNDERFLOW, None)
+
+        if set(packet_bytes) == {224}:
+            #log.info(f"Idle Packet")
+            return (Packet_State.IDLE, None)
+            return
+
         actual_packet = packet_bytes[:6+data_length+1]
-        #print(f"{actual_packet=}")
-        
         header_bits = BitArray(actual_packet[0:6]).bin
-        #print(f"{header_bits=}")
-        
         data = actual_packet[6:]
         decoded_header = {}
         for key in HeaderKeys:
@@ -62,18 +69,12 @@ class CCSDS_Packet():
         decoded_header['data'] = data
         p = CCSDS_Packet(**decoded_header)
         p.encoded_packet = actual_packet
-        #print(p)
-        #print("\n")
-        rest = packet_bytes[6+data_length+1:]
-        if set(rest) == {224}:
-            #print(f"{Fore.RED} \n{rest=}\n {Fore.RESET}")
-            pass
+        #rest = packet_bytes[6+data_length+1:]
+        if p.is_complete():
+            return (Packet_State.COMPLETE, p)
         else:
-            #print(f"\n{rest=}\n")
-            pass
-        #print("\n")
-        return p
-        
+            return (Packet_State.SPILLOVER, p)
+
     def is_complete(self):
         return not bool(self.get_missing())
 
@@ -91,7 +92,7 @@ class CCSDS_Packet():
     def encode(self):
         if self.encoded_packet:
             return self.encoded_packet
-        
+
         new = BitArray()
         for (k, v) in self.primary_header.items():
             size = k.value.start - (k.value.stop-1)
@@ -111,108 +112,63 @@ class AOS_to_CCSDS():
         self.happy_customers = 0
 
     def depacketize(self, data):
+        log.debug("NEW")
+
+        def attempt_packet(data):
+            stat, p = CCSDS_Packet.decode(data)
+            if stat == Packet_State.COMPLETE:
+                log.debug(f"{Fore.GREEN} Got a packet! {Fore.RESET}")
+                accumulated_packets.append(p.encoded_packet)
+                self.bytes_from_previous_frames = bytes()
+                return p.get_next_index()
+
+            elif stat == Packet_State.SPILLOVER:
+                log.debug(f"{Fore.MAGENTA} SPILLOVER missing {p.get_missing()} bytes {Fore.RESET}")
+                self.bytes_from_previous_frames = data
+                return None
+
+            elif stat == Packet_State.UNDERFLOW:
+                log.debug(f"{Fore.RED} UNDERFLOW {data=} {Fore.RESET}")
+                self.bytes_from_previous_frames = data
+                return None
+
+            elif stat == Packet_State.IDLE:
+                log.debug(f"{Fore.YELLOW} IDLE {Fore.RESET}")
+                return None
+
+        def handle_spillover_packet(data):
+            p = attempt_packet(self.bytes_from_previous_frames+data)
+            if p:
+                log.debug(f"{Fore.CYAN} Picked up a packet! {Fore.RESET}")
+
         accumulated_packets = []
         AOS_frame_object = AOSTransFrame(data)
 
-        if AOS_frame_object.is_idle_frame or \
-           AOS_frame_object.get('aos_data_field_type') is not AOSDataFieldType.M_PDU:
-            #log.debug("Dropping idle frame!")
-            print("Drop Idle Frame!")
+        if AOS_frame_object.is_idle_frame:
+            log.debug("Dropping idle frame!")
             return accumulated_packets
-            
+
         if AOS_frame_object.get('mpdu_is_idle_data'):
             print("Idle! Packet!")
             return accumulated_packets
-             
+
         first_header_pointer = AOS_frame_object.get('mpdu_first_hdr_ptr')
         mpdu_packet_zone = AOS_frame_object.get('mpdu_packet_zone')
-        
-        def handle_spillover_packet():
-            #print(f"{Fore.GREEN} {first_header_pointer} {Fore.RESET}")
-            if first_header_pointer == 0:
-                return True
-            
-            if first_header_pointer and self.bytes_from_previous_frames:
-                #print("REEEEEEE SPILLOVER!")
-                #print(f"{first_header_pointer}")
-                q = self.bytes_from_previous_frames + mpdu_packet_zone[:first_header_pointer] # First Header Pointer may be completely  outof frame
-                p = CCSDS_Packet.decode(q)
-                if not p:
-                    log.debug(f"{Fore.CYAN} UNDERFLOW???? {Fore.RESET}")
-                    self.bytes_from_previous_frames += mpdu_packet_zone
-                    return False
-                if p.is_complete():
-                    #print("Completed a spare!")
-                    accumulated_packets.append(p.encoded_packet)
-                    self.bytes_from_previous_frames = bytes()
-                    return True
-                else:
-                    log.debug(f"{Fore.MAGENTA} Overflow??????{Fore.RESET}")
-                    self.bytes_from_previous_frames += mpdu_packet_zone
-                    return False
-            return True
-        
-        def handle_first_packet():
-            first_packet_bytes = mpdu_packet_zone[first_header_pointer:]
-            #print(f'{first_header_pointer=}')
-            p = CCSDS_Packet.decode(first_packet_bytes)
-            if not p: # We don't have enough data to even try a packet
-                log.debug(f"{Fore.CYAN} UNDERFLOW!!!!! {Fore.RESET}")
-                self.bytes_from_previous_frames += first_packet_bytes
-                return None
-            elif p.is_complete():
-                #print("First Packet Good!")
-                accumulated_packets.append(p.encoded_packet)
-                return p
-            else:
-                #print(f"{Fore.MAGENTA} Overflow !!!!!!{Fore.RESET}")
-                self.bytes_from_previous_frames = p.encoded_packet
-                return None
 
-        r = handle_spillover_packet()
-        if not r:
-            log.debug(f'{Fore.YELLOW} SPILLOVER PACKET ERROR! {Fore.RESET}')
-        #     #return accumulated_packets
-        
-        p = handle_first_packet()
-        if not p:
-            #print(f'{Fore.BLUE} FIRST PACKET ERROR! {Fore.RESET}')
-            return accumulated_packets
-            
+        log.debug(f"{first_header_pointer=} ")
+        if first_header_pointer != 0 and self.bytes_from_previous_frames:
+            log.debug(f"Handling spare packet: {first_header_pointer=}")
+            handle_spillover_packet(mpdu_packet_zone[:first_header_pointer])
+
+        pointer = first_header_pointer
         j = 1
-        next_index = first_header_pointer + p.get_next_index()
-        maybe_next_packet = mpdu_packet_zone[next_index:]
-        
-        if set(maybe_next_packet) == {224}:
-            #print("SUNRISE!!!!!!!1")
-            return accumulated_packets
-            
-        while maybe_next_packet:
-            if set(maybe_next_packet) == {224}: # Idle mpdu, special sunrise case
-                #print("SUNRISE")
-                return accumulated_packets
-                
+        while (maybe_next_packet := mpdu_packet_zone[pointer:]):
+            #print(maybe_next_packet.hex())
             #print(f"{j=}")
             j += 1
-
-            #print(f"{maybe_next_packet=}")
-            p = CCSDS_Packet.decode(maybe_next_packet)
-            if not p:
-                log.debug(f"{Fore.CYAN} UNDERFLOW******* {Fore.RESET}")
-                self.bytes_from_previous_frames += maybe_next_packet
+            next_index = attempt_packet(maybe_next_packet)
+            if not next_index:
                 break
-            if p.is_complete():
-                #print("GOTTEM")
-                accumulated_packets.append(p.encoded_packet)
-            else:
-                log.debug(f"{Fore.MAGENTA} Overflow*****{Fore.RESET}")
-                #print(f"{p.get_missing()=}")
-                self.bytes_from_previous_frames += p.encoded_packet
-                break
-                
-            next_index += p.get_next_index()
-            #print(f'next index: {next_index=}')
-            maybe_next_packet = mpdu_packet_zone[next_index:]
-            #print(f"{maybe_next_packet=}")
+            pointer += next_index
 
         return accumulated_packets
