c675ca2218cc9488fe4c7b3e2c4be4a5ba7967e1
jbustos <jessi.r.bustos@jpl.nasa.gov>
2023-02-10 19:25:40 -0800

    G3.0 version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'ait-dsn',
-    version = '2.7.0+snr',
+    version = '3.0.0+snr',
     description  = description,
     long_description = long_description,
     long_description_content_type = 'text/x-rst',
